3ddb27950b892bb6cca9734bd4f6361f8db92616
Wadhwani <dnwadhwani@ieng6-306.ucsd.edu>
2025-10-14 20:36:43 -0700

    Q6 array partitioning for complete/cyclic/block options

---
+++ project_files/project1/Q6/cyclic/fir.cpp
@@ -0,0 +1,60 @@
+/*
+	Filename: fir.cpp
+		FIR lab wirtten for WES/CSE237C class at UCSD.
+		Match filter
+	INPUT:
+		x: signal (chirp)
+
+	OUTPUT:
+		y: filtered output
+
+*/
+
+#include "fir.h"
+
+void fir (
+  data_t *y,
+  data_t x
+  )
+{
+	coef_t c[N] = {10, 11, 11, 8, 3, -3, -8, -11, -11, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -11, -11, -8, -3, 3, 8, 11, 11, 10, 10, 10, 10, 10, 10, 10, 10, 11, 11, 8, 3, -3, -8, -11, -11, -10, -10, -10, -10, -10, -10, -10, -10, -11, -11, -8, -3, 3, 8, 11, 11, 10, 10, 10, 10, 10, 10, 10, 10, 11, 11, 8, 3, -3, -8, -11, -11, -10, -10, -10, -10, -10, -10, -10, -10, -11, -11, -8, -3, 3, 8, 11, 11, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
+	
+	// Write your code here
+	static
+		data_t shift_reg[N];
+		acc_t acc;
+		int i;
+
+        #pragma HLS array_partition variable=c factor=2 cyclic 
+        #pragma HLS array_partition variable=shift_reg factor=2 cyclic 
+	//acc = 0;
+	//Shift_Accum_Loop:
+	//for (i = N - 1; i >= 0; i--){
+	//	if (i == 0) {
+	//		acc += x * c[0];
+	//		shift_reg[0] = x;
+	//	} else {
+	//		shift_reg[i] = shift_reg[i - 1];
+	//		acc += shift_reg[i] * c[i];
+	//	}
+	//}
+	TDL:
+	for (i = N - 1; i > 0; i--){
+		#pragma HLS unroll factor=2
+		shift_reg[i] = shift_reg[i - 1];
+	}
+
+	//acc += x * c[0];
+	shift_reg[0] = x;
+
+	acc = 0;
+	MAC:
+	for (i = N - 1; i >= 0; i--){
+		#pragma HLS unroll factor=2
+		acc += shift_reg[i] * c[i];
+	}
+
+	*y = acc;
+
+}
+
