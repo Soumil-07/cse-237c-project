95632a593b17415efd22117bc19e732fd48d26aa
Soumil Paranjpay <soumil.paranjpay@gmail.com>
2025-10-14 17:43:40 -0700

    Q4

---
+++ project_files/project1/Q4/fir.comp/hls/.autopilot/db/fir.DependenceCheck.tcl
@@ -0,0 +1,10 @@
+
+
+set RtlHierarchyInfo {[
+	{"ID" : "0", "Level" : "0", "Path" : "`AUTOTB_DUT_INST"},
+	{"ID" : "1", "Level" : "1", "Path" : "`AUTOTB_DUT_INST.shift_reg_U"},
+	{"ID" : "2", "Level" : "1", "Path" : "`AUTOTB_DUT_INST.grp_fir_Pipeline_VITIS_LOOP_31_1_fu_62"},
+	{"ID" : "3", "Level" : "2", "Path" : "`AUTOTB_DUT_INST.grp_fir_Pipeline_VITIS_LOOP_31_1_fu_62.fir_int_int_c_U"},
+	{"ID" : "4", "Level" : "2", "Path" : "`AUTOTB_DUT_INST.grp_fir_Pipeline_VITIS_LOOP_31_1_fu_62.mul_6s_32s_32_2_1_U1"},
+	{"ID" : "5", "Level" : "2", "Path" : "`AUTOTB_DUT_INST.grp_fir_Pipeline_VITIS_LOOP_31_1_fu_62.flow_control_loop_pipe_sequential_init_U"}]}
+set DependenceCheckSize 0
